17ffc58cd49eefa2e69e8aa1652a9f20f950434a
Maarten Pronk <git@evetion.nl>
2021-05-17 20:15:02 +0200

    Address minor comments.

--- src/io.jl
+++ src/io.jl
@@ -9,8 +9,8 @@ const fieldmapping = Dict(v => k for (k, v) in AG._FIELDTYPE)
 
 
 
-function read(fn::AbstractString, layer::Union{Integer,AbstractString}=0; args...)
-    ds = AG.read(fn; args...)
+function read(fn::AbstractString, layer::Union{Integer,AbstractString}=0; kwargs...)
+    ds = AG.read(fn; kwargs...)
     layer = AG.getlayer(ds, layer)
     table = AG.Table(layer)
     df = DataFrame(table)
