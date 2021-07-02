19282a22359e367d69a5fa14f3bb63c788d257f6
Documenter.jl <documenter@juliadocs.github.io>
2021-07-02 13:45:46 +0000

    build based on 48eeeff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeoDataFrames","category":"page"},{"location":"#GeoDataFrames","page":"Home","title":"GeoDataFrames","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeoDataFrames]","category":"page"}]
+}
