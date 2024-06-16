be7881905a16230aca3c5f7ea20e6b9d43e165f7
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-16 10:33:14 -0400

    Implement all applicable GeoInterface methods for DataFrames
    But not traits.

--- src/utils.jl
+++ src/utils.jl
@@ -25,3 +25,53 @@ function getcrs(table)
         return nothing
     end
 end
+
+# Override some GeoInterface functions specifically for DataFrames
+
+# These are the basic metadata definitions from which all else follows.
+
+function GeoInterface.crs(table::DataFrame)
+    crs = DataAPI.metadata(table, "GEOINTERFACE:crs", nothing)
+    if isnothing(crs) # fall back to searching for "crs" as a string
+        crs = DataAPI.metadata(table, "crs", nothing)
+    end
+    return crs
+end
+
+function GeoInterface.geometrycolumns(table::DataFrame)
+    gc = DataAPI.metadata(table, "GEOINTERFACE:geometrycolumns", nothing)
+    if isnothing(gc) # fall back to searching for "geometrycolumns" as a string
+        gc = DataAPI.metadata(table, "geometrycolumns", (:geometry,))
+        # TODO: we could search for columns named e.g. `:geometry`, `:geom`. `:shape`, and caps/lowercase versions.
+        # But should we?
+    end
+    return gc
+end
+
+# We don't define DataFrames as feature collections explicitly, since
+# that would complicate handling.  But, we can still implement the 
+# feature interface, for use in generic code.  And dispatch can always
+# handle a DataFrame by fixing the trait in a specialized method.
+
+# Here, we define a feature as a DataFrame row.
+function GeoInterface.getfeature(df::DataFrame, i::Integer)
+    return view(df, i, :)
+end
+# This is simply an optimized method, since we know what we have to do already.
+GeoInterface.getfeature(df::DataFrame) = eachrow(df)
+
+# The geometry is defined as the first of the geometry columns.
+# TODO: how should we choose between the geometry columns?
+function GeoInterface.geometry(row::DataFrameRow)
+    return row[first(GeoInterface.geometrycolumns(row))]
+end
+
+# The properties are all other columns.
+function GeoInterface.properties(row::DataFrameRow)
+    return row[DataFrames.Not(first(GeoInterface.geometrycolumns(row)))]
+end
+
+# Since `DataFrameRow` is simply a view of a DataFrame, we can reach back 
+# to the original DataFrame to get the metadata.
+GeoInterface.geometrycolumns(row::DataFrameRow) = GeoInterface.geometrycolumns(getfield(row, :df)) # get the parent of the row view
+GeoInterface.crs(row::DataFrameRow) = GeoInterface.crs(getfield(row, :df)) # get the parent of the row view
