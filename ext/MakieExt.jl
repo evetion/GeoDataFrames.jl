db1f7086437156b3daf4d15d254c2385d9309b42
Takuya Iwanaga <takuyai@gmail.com>
2024-07-03 11:58:14 +1000

    Initial attempt at supporting variety of geometry types
    Add newline at EOF

--- ext/MakieExt.jl
+++ ext/MakieExt.jl
@@ -4,7 +4,27 @@ using GeoDataFrames
 using Makie, GeoMakie
 
 import GeoDataFrames.DataFrame
+import GeoMakie.GeoInterface as GI
+using GeoMakie.GeometryBasics
 
+
+"""
+    plot(gdf::DataFrame)
+
+Plot geometries from first column found to hold geometry data.
+
+# Arguments
+- `gdf` : GeoDataFrame
+"""
+function GeoDataFrames.plot(gdf::DataFrame)
+    col = try
+        first(GI.geometrycolumns(gdf))
+    catch err
+        rethrow(err)
+    end
+
+    return GeoDataFrames.plot(gdf, col)
+end
 
 """
     plot(gdf::DataFrame, geom_col::Symbol)
@@ -27,13 +47,34 @@ function GeoDataFrames.plot(gdf::DataFrame, geom_col::Symbol)
         aspect=AxisAspect(0.75)
     )
 
-    plottable = GeoMakie.geo2basic(gdf[!, geom_col])
-    poly!(ga, plottable)
+    trait = GI.trait(gdf[1, geom_col])
+    if trait == GI.PointTrait()
+        _plot!(ga, GeoMakie.geo2basic(gdf[!, geom_col]))
+    else
+        # Try plotting as multipolygon
+        _plot!(ga, GeoMakie.to_multipoly(gdf[!, geom_col]))
+    end
 
     xlims!(ga)
     ylims!(ga)
 
     return f
+end
+
+function _plot!(ga::GeoAxis, data::Vector{<:GeometryBasics.Point})::Nothing
+    scatter!(ga, data)
+
+    return nothing
+end
+function _plot!(ga::GeoAxis, data::Vector{<:Vector{<:T}})::Nothing where {T<:GeometryBasics.MultiPolygon}
+    poly!(ga, data)
+
+    return nothing
+end
+function _plot!(ga::GeoAxis, data::Vector{<:T})::Nothing where {T<:GeometryBasics.MultiPolygon}
+    poly!(ga, data)
+
+    return nothing
 end
 
 end
