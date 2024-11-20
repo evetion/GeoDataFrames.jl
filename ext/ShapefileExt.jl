c522d269622c8ebc796f70364314ec8aa6169389
Maarten Pronk <git@evetion.nl>
2024-11-20 11:51:44 +0100

    Let's also add files.

---
+++ ext/ShapefileExt.jl
@@ -0,0 +1,13 @@
+module ShapefileExt
+using GeoDataFrames: ShapefileDriver, GeoDataFrames
+using Shapefile
+
+function GeoDataFrames.read(::ShapefileDriver, fname::AbstractString; kwargs...)
+    GeoDataFrames.DataFrame(Shapefile.Table(fname; kwargs...); copycols = false)
+end
+
+function GeoDataFrames.write(::ShapefileDriver, fname::AbstractString, data; kwargs...)
+    Shapefile.write(fname, data; kwargs...)
+end
+
+end
