307eff1b4d1d9f6a5277887c394b8d018a418623
Maarten Pronk <git@evetion.nl>
2025-05-07 16:45:15 +0200

    Update test files.
    Revert delete of null.gpkg

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,43 +17,43 @@ REPO_URL = "https://github.com/yeesian/ArchGDALDatasets/blob/master/"
 
 remotefiles = [
     (
-        "https://github.com/yeesian/ArchGDALDatasets/blob/master/ospy/data1/sites.dbf?raw=true",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/sites.dbf",
         "7df95edea06c46418287ae3430887f44f9116b29715783f7d1a11b2b931d6e7d",
     ),
     (
-        "https://github.com/yeesian/ArchGDALDatasets/blob/master/ospy/data1/sites.prj?raw=true",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/sites.prj",
         "81fb1a246728609a446b25b0df9ede41c3e7b6a133ce78f10edbd2647fc38ce1",
     ),
     (
-        "https://github.com/yeesian/ArchGDALDatasets/blob/master/ospy/data1/sites.sbn?raw=true",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/sites.sbn",
         "198d9d695f3e7a0a0ac0ebfd6afbe044b78db3e685fffd241a32396e8b341ed3",
     ),
     (
-        "https://github.com/yeesian/ArchGDALDatasets/blob/master/ospy/data1/sites.sbx?raw=true",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/sites.sbx",
         "49bbe1942b899d52cf1d1b01ea10bd481ec40bdc4c94ff866aece5e81f2261f6",
     ),
     (
-        "https://github.com/yeesian/ArchGDALDatasets/blob/master/ospy/data1/sites.shp?raw=true",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/sites.shp",
         "69af5a6184053f0b71f266dc54c944f1ec02013fb66dbb33412d8b1976d5ea2b",
     ),
     (
-        "https://github.com/yeesian/ArchGDALDatasets/blob/master/ospy/data1/sites.shx?raw=true",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/sites.shx",
         "1f3da459ccb151958743171e41e6a01810b2a007305d55666e01d680da7bbf08",
     ),
     (
-        "https://github.com/opengeospatial/geoparquet/raw/v1.0.0/examples/example.parquet",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/example.parquet",
         "3dc1a3df76290cc62aa6d7aa6aa00d0988b3157ee77a042167b3f8302d05aa6c",
     ),
     (
-        "https://raw.githubusercontent.com/geoarrow/geoarrow-data/v0.1.0/example/example-multipolygon_z.arrow",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/example-multipolygon_z.arrow",
         "0cd4d7c5611581a5ae30fbbacc6f733a7da2ae78ac02ef8cffeb1e2d4f39b91c",
     ),
     (
-        "https://storage.googleapis.com/open-geodata/linz-examples/nz-buildings-outlines.parquet",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/nz-buildings-outlines.parquet",
         "b64389237b9879c275aec4e47f0e6be8fdd5d64437a05aef10839f574efc5dbc",
     ),
     (
-        "https://github.com/bjornharrtell/flatgeobuf/blob/master/test/data/countries.fgb?raw=true",
+        "https://github.com/evetion/GeoDataFrames.jl/releases/download/v0.1.0/countries.fgb",
         "d8dc3baf855320d10c6a662bf1171273849dd8a0935066b5b7b8dd83b3484cb3",
     ),
 ]
