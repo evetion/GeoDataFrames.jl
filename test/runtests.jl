3a57671c3b1e61c4abfc3d0e3270a302a78d4179
Maarten Pronk <git@evetion.nl>
2026-07-31 15:44:18 +0200

    Dump CSV+Parquet on Windows tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -680,12 +680,14 @@ end
     ]
     if Sys.iswindows()
         # GDAL.jl#146, GeoParquet on Windows is broken
+        push!(broken_combos, (GDF.CSVDriver(), GDF.GeoParquetDriver()))
         push!(broken_combos, (GDF.ArchGDALDriver(), GDF.GeoParquetDriver()))
         push!(broken_combos, (GDF.ShapefileDriver(), GDF.GeoParquetDriver()))
         push!(broken_combos, (GDF.GeoJSONDriver(), GDF.GeoParquetDriver()))
         push!(broken_combos, (GDF.FlatGeobufDriver(), GDF.GeoParquetDriver()))
         push!(broken_combos, (GDF.GeoArrowDriver(), GDF.GeoParquetDriver()))
         push!(broken_combos, (GDF.GeoParquetDriver(), GDF.ArchGDALDriver()))  # not sure why this one fails
+        push!(broken_combos, (GDF.GeoParquetDriver(), GDF.CSVDriver()))  # not sure why this one fails
         push!(broken_combos, (GDF.GeoParquetDriver(), GDF.ShapefileDriver()))  # not sure why this one fails
         push!(broken_combos, (GDF.GeoParquetDriver(), GDF.GeoJSONDriver()))  # not sure why this one fails
         push!(broken_combos, (GDF.GeoParquetDriver(), GDF.FlatGeobufDriver()))  # not sure why this one fails
